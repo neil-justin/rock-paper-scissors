17f3b0560222c7a92b3815b6337b73a5f9bbeea4
Neil Justin <mallarineil9@gmail.com>
2022-06-24 17:06:22 +0800

    Add a function that plays a single round

---
+++ main.js
@@ -0,0 +1,60 @@
+function computerPlay() {
+    let computerChoice = Math.ceil(Math.random() * 3);
+
+    switch (computerChoice) {
+        case 1:
+            return ('Rock');
+        case 2:
+            return ('Paper');
+        case 3:
+            return ('Scissors');
+    }
+}
+
+function playerPlay() {
+    let playerChoice = prompt('Enter your weapon choice', 'Rock, Paper, or Scissors?');
+    let fixInvalidInput = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
+    return fixInvalidInput;
+}
+
+let computerSelection = computerPlay();
+let playerSelection = playerPlay();
+
+function playRound(computerSelection, playerSelection) {
+    if ((playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+        (playerSelection === 'Paper' && computerSelection === 'Rock') ||
+        (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
+        return playerWin();
+    } else if (playerSelection === computerSelection) {
+        return noWinner();
+    } else {
+        return cpuWin();
+    }
+}
+
+let playerScore = 0;
+let cpuScore = 0;
+
+function playerWin() {
+    playerScore += 1;
+    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    return displayScore();
+
+}
+
+function noWinner() {
+    console.log('It\s a tie! You both use the same weapon.');
+    return displayScore();
+}
+
+function cpuWin() {
+    cpuScore += 1;
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    return displayScore();
+}
+
+function displayScore() {
+    return `Player score: ${playerScore} | Computer score: ${cpuScore}`;
+}
+
+console.log((playRound(computerSelection, playerSelection)));
